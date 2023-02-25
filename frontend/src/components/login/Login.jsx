49f9a553811165b9302afc0510ab1bbc0f5818d5
Uyen Doan <catuyen99@gmail.com>
2023-02-25 12:21:55 -0600

    Changed fonts

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -4,8 +4,15 @@ import './Login.css';
 const Login = () => {
 
     return (
-        <div>
-            
+        <div className="loginForm w-full max-w-xs">
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    Username
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+                </div>
+            </form>
         </div>
     )
 }
