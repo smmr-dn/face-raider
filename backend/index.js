d6358e2b57d4f8f73f40cd39235c1a83eaa9cc93
John Manacup <john.manacup@gmail.com>
2023-02-25 17:53:48 -0600

    updated register api

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+import { StorageClient } from "@supabase/storage-js";
+
 import dotenv from "dotenv";
 import express from "express";
 
@@ -9,10 +11,22 @@ const supabaseUrl = "https://tghtmxvonwrxpqauhixi.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+//supabase storage setup
+
+// const supabaseStorageUrl =
+//     "https://tghtmxvonwrxpqauhixi.supabase.co/storage/v1";
+// const supabaseStorageKey = process.env.SUPABASE_STORAGE_KEY;
+// const storageClient = new StorageClient(supabaseStorageUrl, {
+//     apikey: supabaseStorageKey,
+//     Authorization: `Bearer ${supabaseStorageKey}`,
+// });
+
 //port setup
 const port = process.env.PORT;
 
 const app = express();
+
+app.use(express.json({ limit: "50mb" }));
 
 app.get("/", async (req, res) => {
     res.send("Hello World");
@@ -26,7 +40,27 @@ app.get("/getUser", async (req, res) => {
 // POST Requests
 
 app.post("/register", async (req, res) => {
-    res.send(await supabase.auth.signUp(req.body));
+    //cut first
+
+    //sign up the user for authentication
+    let { data, error } = await supabase.auth.signUp(req.body);
+
+    if (error) {
+        res.send(error);
+    } else {
+        //add it with the picture path
+        const { data, error } = await supabase.from("Users").insert([
+            {
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                r_number: req.body.r_number,
+                email: req.body.email,
+                picture_path: req.body.image,
+            },
+        ]);
+
+        res.send("done");
+    }
 });
 
 app.post("/login", async (req, res) => {
