14adb3aad13006d5214c1789058930db96b5a916
John Manacup <john.manacup@gmail.com>
2023-02-25 14:13:32 -0600

    added RESTful API's

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,44 @@ const app = express();
 app.get("/", async (req, res) => {
     res.send("Hello World");
 });
+
+// GET Requests
+app.get("/getUser", async (req, res) => {
+    res.send(await supabase.auth.getUser());
+});
+
+// POST Requests
+
+app.post("/register", async (req, res) => {
+    res.send(await supabase.auth.signUp(req.body));
+});
+
+app.post("/login", async (req, res) => {
+    res.send(await supabase.auth.signInWithPassword(req.body));
+});
+
+app.post("/logout", async (req, res) => {
+    res.send(await supabase.auth.signOut());
+});
+
+app.post("/joinCourse", async (req, res) => {
+    await supabase.from("PeopleInCourse").insert([
+        {
+            user_id: req.query.user_id,
+            course_id: req.query.course_id,
+        },
+    ]);
+});
+
+app.post("/checkIn", async (req, res) => {
+    await supabase.from("Verify").insert([
+        {
+            user_id: req.query.user_id,
+            verification_date: new Date(),
+        },
+    ]);
+});
+
 app.listen(port, () => {
     console.log(`Working at port: ${port}`);
 });
