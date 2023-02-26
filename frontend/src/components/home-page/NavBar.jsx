65608191806c1d6959802512d2eb51c807ca92f6
Thu Nguyen <66782390+ThuNguyen-198@users.noreply.github.com>
2023-02-26 02:24:27 -0600

    add user account page

--- frontend/src/components/home-page/NavBar.jsx
+++ frontend/src/components/home-page/NavBar.jsx
@@ -5,20 +5,24 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="navbar">
-      {/* <Link to="/"> */}
-      <Link to="/">
+      <Link className="auth-item" to="/">
         <div className="app-logo">
           <img className="logo-nav" src={logo} />
           <p className="app-name-nav">Face Raider</p>
         </div>
       </Link>
-      {/* </Link> */}
       <ul className="auth">
         <li>
-          <Link to="/signup"> Register</Link>
+          <Link className="auth-item" to="/signup">
+            {" "}
+            Register
+          </Link>
         </li>
-        <li>
-          <Link to="/login"> Login</Link>
+        <li className="auth-item">
+          <Link className="auth-item" to="/login">
+            {" "}
+            Login
+          </Link>
         </li>
       </ul>
     </div>
