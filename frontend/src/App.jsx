c8ade9eb1773ef053c9ba84ef9bb7e49aa2fe064
Uyen Doan <catuyen99@gmail.com>
2023-02-26 02:52:42 -0600

    Updated nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,15 @@ import ScanOptions from "./components/home-page/ScanOptions";
 import Login from "./components/login/Login";
 import Signup from "./components/signup/Signup";
 import Account from "./components/account/Account";
+import AttendanceHome from "./components/home-page/AttendanceHome";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      {/* <HomePage /> */}
-      <Account />
+      <HomePage />
+
     </div>
   );
 }
