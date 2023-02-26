57e29095657711d3a1a6dc3a3115f42423dd944f
Uyen Doan <catuyen99@gmail.com>
2023-02-26 01:09:18 -0600

    Added login and signup backend

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo-icon.png";
+import axios from "axios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    const data = {email, password};
+
+    try {
+        await axios.post("http://localhost:5000/login", data, {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then((data) => {
+            if(data){
+                
+                window.location.href = '/';
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
   return (
+    
     <div className="form-container">
       <img src={logo} />
       <h1>Face Raider</h1>
       <h2>LOGIN</h2>
-      <form className="form">
-        <div className="form-inputs">
-          <input
-            id="login-signup-input"
-            className="text-input"
-            type="text"
-            placeholder="R"
-          ></input>
-        </div>
+      <form className="form" id="login-form" onSubmit={handleSubmit}>
+    
         <div className="form-inputs">
           <input
             id="login-signup-input"
@@ -36,7 +55,7 @@ const Login = () => {
         </div>
       </form>
       <div className="button">
-        <button className="button-81" id="submit-btn" type="submit">
+        <button className="button-81" form="login-form" id="submit-btn" type="submit">
           LOGIN
         </button>
       </div>
